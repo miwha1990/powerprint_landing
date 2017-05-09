aa2a3e72bc007c6ea52a76eb9dc599f94ee04777
miwha1990 <yandex@i.ua>
2017-05-09 13:23:18 +0300

    форма регистрации с валидацией

--- js/main.js
+++ js/main.js
@@ -75,13 +75,13 @@
 
             var one_start = 0;
 
-            setInterval(function(){
+            /*setInterval(function(){
                 ++one_start;
                 var one_css = {
                     'backgroundPosition': (Math.sin(one_start * 0.0025) * 100) + 'px ' + Math.cos(one_start * 0.0025) * 100 + "px"
                 };
                 one.css(one_css);
-            }, 1);
+            }, 1);*/
 
         /*
          Second block animation
@@ -154,12 +154,126 @@
         Login-register modal
         */
             var loginButton = $('.log');
-            loginButton.on('click', function(){
-                $('.modal-login').addClass('modal-appearing');
+            var logModal = $('#modal1');
+            var regModal = $('#modal2');
+            var wrapperModal = $('#wrapper');
+            loginButton.on('click', function(e){
+                if(wrapperModal.html().trim() === '1')
+                   wrapperModal.html(logModal.html());
+                wrapperModal.modal({
+                    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+                    opacity: 0, // Opacity of modal background
+                    inDuration: 300, // Transition in duration
+                    outDuration: 200, // Transition out duration
+                    startingTop: '4%', // Starting top style attribute
+                    endingTop: '10%', // Ending top style attribute
+                    ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+                    },
+                    complete: function() {} // Callback for Modal close
+                });
             });
-            $('.modal-login span').click(function(){
-                $('.modal-login').removeClass('modal-appearing');
+
+            wrapperModal.delegate('#register', "click",  function() {
+                wrapperModal.find('.modal-content, .modal-footer').animate({
+                    opacity: 0
+                }, 500, function() {
+                    wrapperModal.addClass('reg-form').html(regModal.html());
+                });
             });
+            wrapperModal.delegate('#login', "click",  function() {
+                wrapperModal.find('.modal-content, .modal-footer').animate({
+                    opacity: 0
+                }, 500, function() {
+                    wrapperModal.removeClass('reg-form').html(logModal.html());
+                });
+            });
+
+        /*
+        Login-register validation
+         */
+            wrapperModal.delegate('form', "submit",  function(e) {
+                e.preventDefault();
+                var thisForm = $(this);
+                var name = thisForm.find('input[type="text"]');
+                var email = thisForm.find('input[type="email"]');
+                var password = thisForm.find('input[type="password"]');
+                var errors = [];
+                if(name.length){
+                    if(name.val().length < 6 || name.val().length >40){
+                        errors.push({'target':'name', 'message':'<strong>Имя</strong> должно быть от 5 до 40 символов.'});
+                        name.addClass('error');
+                    } else {
+                        name.removeClass('error').addClass('valid');
+                    }
+                }
+                if (!validateEmail(email.val())) {
+                    errors.push({'target':'email', 'message':'<strong>Почта</strong> невалидна.'});
+                    email.addClass('error');
+                } else {
+                    email.removeClass('error').addClass('valid');
+                }
+                if(validatePassword( password )){
+                    errors.push({'target':'password', 'message':validatePassword( password )});
+                    password.addClass('error');
+                } else {
+                    password.removeClass('error').addClass('valid');
+                }
+                if(errors.length){
+                    var errorsList = '';
+                    var modalAllertModal = $('.modal-alerts');
+                    errors.forEach(function(e){
+                        errorsList += '<li>'+e.message+'</li>';
+                    });
+                    modalAllertModal.addClass('rollup').find('ul').html(errorsList);
+                    setTimeout(function(){
+                        modalAllertModal.removeClass('rollup');
+                    }, 5000);
+                    return false;
+                } else {
+                    $.ajax({
+                        url: thisForm.attr('action'),
+                        method: 'POST',
+                        data: thisForm.serialize(),
+                        success: function(e){
+                            debugger;
+
+                        },
+                        error: function(e){
+                            debugger;
+                        }
+                    }).done(function() {
+                        $( this ).addClass( "done" );
+                    });
+
+                }
+            });
+            function validateEmail(email) {
+                var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                return re.test(email);
+            }
+            function validatePassword( p ) {
+                var errors = [];
+                if(p.length > 1){
+                    if(p[0].value !== p[1].value)
+                        errors.push("<strong>Пароли</strong> не совпадают!");
+                    p = p[0].value;
+                } else { p = p.val()}
+
+                if (p.length < 6) {
+                    errors.push("<strong>Пароль</strong> должен быть минимум 6 символов.");
+                }
+                if (p.search(/[a-z]/i) < 0) {
+                    errors.push("<strong>Пароль</strong> должен содержать хотя бы 1 букву.");
+                }
+                if (p.search(/[0-9]/) < 0) {
+                    errors.push("<strong>Пароль</strong> должен содержать хотя бы 1 цифру.");
+                }
+                if (errors.length > 0) {
+                    return errors.join("<br>");
+                }
+                return false;
+            }
+
     }); // end of document ready
 })(jQuery);// end of jQuery name space
 
